32d7f1abb41f7db3b7f4bd48778796f35bb37fc2
chi-wei-lien <lien2@purdue.edu>
2021-10-23 08:03:41 -0400

    finish landing page demo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ function App() {
       <div className="container-fluid hero-container">
         <div className="hero-image">
           <div className="hero-text">
-            <h1>hi</h1>
+            <h1>Y Budge</h1>
+            <p>Keep your spending responsible with automatic social media posts when you go out</p>
+            <button type="button" class="btn btn-outline-light">Read More</button>
           </div>
         </div>
       </div>
