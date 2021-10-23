0aab1b62ae8d5b80866d77909a0770e2259959ab
chi-wei-lien <lien2@purdue.edu>
2021-10-23 00:08:57 -0400

    navbar

--- ybudge/src/App.js
+++ ybudge/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
+import Navbar from './components/navbar.component';
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        hi
-      </div>
-    </Router>
+    <div className="container">
+      <Router>
+        <Navbar>
+          
+        </Navbar>
+      </Router>
+    </div>
   );
 }
 
