d8ad850c9ad3daee651973f889be3ad6c7c16699
chi-wei-lien <lien2@purdue.edu>
2021-10-22 22:14:04 -0400

    mongodb connection established, create models folder, create routes folder

--- ybudge/backend/server.js
+++ ybudge/backend/server.js
@@ -10,18 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
 
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log("MongoDB database connection established successfully");
-// })
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
 
-// const exercisesRouter = require('./routes/exercises');
 // const usersRouter = require('./routes/users');
 
-// app.use('/exercises', exercisesRouter);
 // app.use('/users', usersRouter);
 
 app.listen(port, () => {
